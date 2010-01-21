710d693bfa58149a95b1a519ec59625fe187dbe4
Andrey Breslav <abreslav@gmail.com>
2010-01-21 16:26:19 +0000

    formatting

--- ee.stacc.productivity.edsl.model/src/ee/stacc/productivity/edsl/string/StringConstant.java
+++ ee.stacc.productivity.edsl.model/src/ee/stacc/productivity/edsl/string/StringConstant.java
@@ -3,10 +3,9 @@
  */
 package ee.stacc.productivity.edsl.string;
 
-
 public class StringConstant implements IAbstractString {
 	private final String constant;
-	
+
 	public StringConstant(String constant) {
 		this.constant = constant;
 	}
@@ -14,17 +13,17 @@ public class StringConstant implements IAbstractString {
 	public String toString() {
 		if (constant.length() == 0) {
 			return "\"\"";
-		}
-		else {
+		} else {
 			return "\"" + constant.replaceAll("\\\"", "\\\\\\\"") + "\"";
 		}
 	}
-	
+
 	public String getConstant() {
 		return constant;
 	}
 
-	public <R, D> R accept(IAbstractStringVisitor<? extends R,? super D> visitor, D data) {
+	public <R, D> R accept(
+			IAbstractStringVisitor<? extends R, ? super D> visitor, D data) {
 		return visitor.visitStringConstant(this, data);
 	};
 }
