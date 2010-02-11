f4caf1297301c318ed012a1af4c92f215fc28659
Aivar Annamaa <aivar.annamaa@gmail.com>
2010-02-11 12:33:58 +0000

    cleaned a bit

--- ee.stacc.productivity.edsl.gui/src/ee/stacc/productivity/edsl/gui/Activator.java
+++ ee.stacc.productivity.edsl.gui/src/ee/stacc/productivity/edsl/gui/Activator.java
@@ -26,28 +26,6 @@ public class Activator extends AbstractUIPlugin  {
 	public void start( final BundleContext context ) throws Exception {
 	        super.start( context );
 	        plugin = this;
-	        
-	        /* ei tööta:
-	        ICommandService commandService = (ICommandService)plugin.getWorkbench().getService( ICommandService.class );
-	        commandService.addExecutionListener( new IExecutionListener() {
-
-	                public void notHandled( final String commandId, final NotHandledException exception ) {}
-
-	                public void postExecuteFailure( final String commandId, final ExecutionException exception ) {}
-
-	                public void postExecuteSuccess( final String commandId, final Object returnValue ) {
-	                        if ( commandId.equals( "org.eclipse.ui.file.save" ) ) {
-	                        	System.err.println("jura");
-	                                // add in your action here...
-	                                // personally, I would use a custom preference page, 
-	                                // but hard coding would work ok too
-	                        }
-	                }
-
-	                public void preExecute( final String commandId, final ExecutionEvent event ) {}
-
-	        } );
-	        */
 	}
 	
 	public void stop(BundleContext context) throws Exception {
