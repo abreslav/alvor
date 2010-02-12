1d71547a31f6d4905df96796517ae69b96213560
Andrey Breslav <abreslav@gmail.com>
2010-02-12 15:55:55 +0000

    Hotspot patterns are read from a property file

--- ee.stacc.productivity.edsl.crawler/src/ee/stacc/productivity/edsl/main/JavaElementChecker.java
+++ ee.stacc.productivity.edsl.crawler/src/ee/stacc/productivity/edsl/main/JavaElementChecker.java
@@ -1,6 +1,7 @@
 package ee.stacc.productivity.edsl.main;
 
-import java.util.Arrays;
+import java.util.ArrayList;
+import java.util.Collections;
 import java.util.List;
 import java.util.Map;
 
@@ -22,28 +23,68 @@ import ee.stacc.productivity.edsl.crawler.NodeSearchEngine;
  */
 public class JavaElementChecker {
 
+	private static final String HOTSPOTS = "hotspots";
+
+	/*
+	 * The map must contain an entry 
+	 * hotspots=classname,methodname,index;classname,methodname,index;...
+	 * E.g.:
+	 * hotspots=java.util.Connection,prepareStatement,1;blah.blah.Blah,blah,5
+	 * Trailing ';' is not required 
+	 */
 	public List<IStringNodeDescriptor> findHotspots(IJavaElement scope, Map<String, Object> options) {
-		NodeSearchEngine.clearCache();
-		List<IStringNodeDescriptor> descriptors = AbstractStringEvaluator.evaluateMethodArgumentAtCallSites(Arrays.asList(
-				new NodeRequest("org.springframework.jdbc.core.simple.SimpleJdbcTemplate", "queryForInt", 1),
-				new NodeRequest("org.springframework.jdbc.core.simple.SimpleJdbcTemplate", "queryForObject", 1),
-				new NodeRequest("org.springframework.jdbc.core.simple.SimpleJdbcTemplate", "queryForLong", 1),
-				new NodeRequest("org.springframework.jdbc.core.simple.SimpleJdbcTemplate", "query", 1),
-				new NodeRequest("org.araneaframework.backend.list.helper.ListSqlHelper", "setSqlQuery", 1),
-				new NodeRequest("java.sql.Connection", "prepareStatement", 1)
-			), scope, 0);
-		return descriptors;
-	}
-	
-	public void checkHotspots(List<IStringNodeDescriptor> hotspots, ISQLErrorHandler errorHandler, List<IAbstractStringChecker> checkers, Map<String, Object> options) {
-	
-		for (IStringNodeDescriptor stringNodeDescriptor : hotspots) {
-			System.out.println(stringNodeDescriptor.getAbstractValue());
+		List<NodeRequest> requests = parseNodeRequests(options);
+		if (requests.isEmpty()) {
+			throw new IllegalArgumentException("No hotspots found");
 		}
-		
-		
+		NodeSearchEngine.clearCache();
+		return AbstractStringEvaluator.evaluateMethodArgumentAtCallSites(requests, scope, 0);
+	}
+
+	public void checkHotspots(
+			List<IStringNodeDescriptor> hotspots, 
+			ISQLErrorHandler errorHandler, 
+			List<IAbstractStringChecker> checkers, 
+			Map<String, Object> options) {
+	
 		for (IAbstractStringChecker checker : checkers) {
 			checker.checkAbstractStrings(hotspots, errorHandler, options);
 		}
 	}
+
+	private List<NodeRequest> parseNodeRequests(Map<String, Object> options) {
+		if (options == null) {
+			return Collections.emptyList();
+		}
+		Object option = options.get(HOTSPOTS);
+		if (option == null) {
+			return Collections.emptyList();
+		}
+		String allHotspots = option.toString();
+		
+		System.out.println("Hotspots:");
+		List<NodeRequest> requests = new ArrayList<NodeRequest>();
+		for (String hotspot : allHotspots.split(";")) {
+			if (hotspot.length() == 0) {
+				continue;
+			}
+			String[] split = hotspot.split(",");
+			if (split.length != 3) {
+				System.err.println("Malformed hotspot: " + hotspot);
+				continue;
+			}
+			String className = split[0];
+			String methodName = split[1];
+			String argumentIndex = split[2];
+			try {
+				int index = Integer.parseInt(argumentIndex);
+				NodeRequest nodeRequest = new NodeRequest(className, methodName, index);
+				requests.add(nodeRequest);
+			} catch (NumberFormatException e) {
+				System.err.println("Number format error: " + argumentIndex);
+			}
+		}
+		return requests;
+	}
+	
 }
