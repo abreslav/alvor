44896ccf9de92ff059748eb3f3815f3b700da3b9
Andrey Breslav <abreslav@gmail.com>
2010-03-12 11:18:15 +0000

    Search experiments

--- ee.stacc.productivity.edsl.cmdline/src/ee/stacc/productivity/edsl/cmdline/EDSLApplication.java
+++ ee.stacc.productivity.edsl.cmdline/src/ee/stacc/productivity/edsl/cmdline/EDSLApplication.java
@@ -28,11 +28,29 @@ import ee.stacc.productivity.edsl.main.OptionLoader;
  */
 public class EDSLApplication implements IApplication {
 
+	private ILog log;
+	private final ISQLErrorHandler errorHandler = new ISQLErrorHandler() {
+		
+		@Override
+		public void handleSQLError(
+				String errorMessage,
+				IPositionDescriptor descriptor) {
+			log.error(errorMessage);
+		}
+		
+		@Override
+		public void handleSQLWarning(
+				String message,
+				INodeDescriptor descriptor) {
+			log.error(message);
+		}
+		
+	};
 	
 	@Override
 	public Object start(IApplicationContext context) throws Exception {
 		Logs.configureFromStream(EDSLApplication.class.getClassLoader().getResourceAsStream("logging.properties"));
-		final ILog log = Logs.getLog(EDSLApplication.class);
+		log = Logs.getLog(EDSLApplication.class);
 		log.message("Projects found:");
 		IWorkspaceRoot root = ResourcesPlugin.getWorkspace().getRoot();
 		IJavaModel javaModel = JavaCore.create(root);
@@ -52,29 +70,18 @@ public class EDSLApplication implements IApplication {
 						log.message(sf.getElementName());
 						JavaElementChecker projectChecker = new JavaElementChecker();
 						
-						long time = System.currentTimeMillis();
 						Map<String, Object> options = OptionLoader.getElementSqlCheckerProperties(iJavaElement);
-						List<INodeDescriptor> hotspots = projectChecker.findHotspots(sf, options);
-						time = System.currentTimeMillis() - time;
-						log.format("%d\n", time);
+						List<INodeDescriptor> hotspots = findHotspots(sf,
+								projectChecker, options);
+//						int count = 5;
+//						long totalTime = 0;
+//						for (int i = 0; i < count; i++) {
+//							hotspots = findHotspots(sf,	projectChecker, options);
+//							totalTime += time;
+//						}
+//						log.format("Average time: %f", (totalTime + 0.0) / count);
 						projectChecker.processHotspots(hotspots, 
-								new ISQLErrorHandler() {
-
-									@Override
-									public void handleSQLError(
-											String errorMessage,
-											IPositionDescriptor descriptor) {
-										log.error(errorMessage);
-									}
-									
-									@Override
-									public void handleSQLWarning(
-											String message,
-											INodeDescriptor descriptor) {
-										log.error(message);
-									}
-									
-								}, 
+								errorHandler, 
 								AbstractStringCheckerManager.INSTANCE.getCheckers(),
 								options
 							);
@@ -85,8 +92,40 @@ public class EDSLApplication implements IApplication {
 		return IApplication.EXIT_OK;
 	}
 
+	long time;
+	private List<INodeDescriptor> findHotspots(IPackageFragmentRoot sf,
+			JavaElementChecker projectChecker, Map<String, Object> options) {
+		long time = System.currentTimeMillis();
+		List<INodeDescriptor> hotspots = projectChecker.findHotspots(sf, options);
+		time = System.currentTimeMillis() - time;
+		this.time = time;
+		log.format("%d for %d hotspots\n", time, hotspots.size());
+		return hotspots;
+	}
+
 	@Override
 	public void stop() {
 		// nothing to do
 	}
+	
+//	private void regularSeqrch(IPackageFragmentRoot sf)
+//			throws JavaModelException {
+//		SearchPattern subPattern = SearchPattern.createPattern("org.springframework.jdbc.core.simple.SimpleJdbcTemplate.queryForObject", 
+//				IJavaSearchConstants.METHOD, IJavaSearchConstants.REFERENCES, 
+//				SearchPattern.R_ERASURE_MATCH | SearchPattern.R_CASE_SENSITIVE);
+//		long t = System.currentTimeMillis();
+//		SearchResultGroup[] search = RefactoringSearchEngine.search(
+//				subPattern, 
+//				SearchEngine.createJavaSearchScope(new IJavaElement[] {sf}, IJavaSearchScope.SOURCES), 
+//				null, 
+//				null);
+//		t = System.currentTimeMillis() - t;
+//		int c = 0;
+//		for (SearchResultGroup searchResultGroup : search) {
+//			SearchMatch[] searchResults = searchResultGroup.getSearchResults();
+//			c += searchResults.length;
+//		}
+//		System.out.println(t + " for " + c);
+//	}
+	
 }
