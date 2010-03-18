7081e6e929636e2df42e5f5fdd1e0eb162e4d03e
Aivar Annamaa <aivar.annamaa@gmail.com>
2010-03-18 20:29:47 +0000

    new tests

--- EDSLTestProject/src/ee/stacc/productivity/edsl/testproject/SomeSubclass.java
+++ EDSLTestProject/src/ee/stacc/productivity/edsl/testproject/SomeSubclass.java
@@ -8,8 +8,32 @@ public class SomeSubclass extends SomeSuperclass {
 	private static String STR2 = STR1 + " blaablaa";
 	
 	@SuppressWarnings("null")
-	public static void testFields() throws SQLException {
+	public static void testInheritedFields() throws SQLException {
 		Connection conn=null;
 		conn.prepareStatement(STR2);
+	}
+	
+	@SuppressWarnings("null")
+	public void testInheritedMethods() throws SQLException {
+		Connection conn=null;
+		
+		SomeSubclass sub1 = new SomeSubclass();
+		SomeSuperclass sub2 = new SomeSubclass();
+		SomeSuperclass sup1 = new SomeSuperclass();
+		
+		conn.prepareStatement(getSomeStr());
+		conn.prepareStatement(sub1.getSomeStr());
+		conn.prepareStatement(sub2.getSomeStr());
+		conn.prepareStatement(sup1.getSomeStr());
+		
+		conn.prepareStatement(SomeSuperclass.getSomeStr(3));
+	}
+	
+	String getSomeStr() {
+		return "SomeSubclass.getSomeStr()";
+	}
+	
+	static String getSomeStr(int i) {
+		return "SomeSubclass.getSomeStr(int i)";
 	}
 }
--- EDSLTestProject/src/ee/stacc/productivity/edsl/testproject/SomeSuperclass.java
+++ EDSLTestProject/src/ee/stacc/productivity/edsl/testproject/SomeSuperclass.java
@@ -2,4 +2,12 @@ package ee.stacc.productivity.edsl.testproject;
 
 public class SomeSuperclass {
 	protected static final String STR1 = "hohoho";
+	
+	String getSomeStr() {
+		return "SomeSuperclass.getSomeStr()";
+	}
+	
+	static String getSomeStr(int i) {
+		return "SomeSuperclass.getSomeStr(int i)";
+	}
 }
