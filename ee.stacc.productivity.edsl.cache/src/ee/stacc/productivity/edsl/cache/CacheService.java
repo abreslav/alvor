2e515ee2e8a8cbb87013d160c161d329a0f9b35e
Andrey Breslav <abreslav@gmail.com>
2010-04-09 12:03:38 +0000

    WS-based cache

--- ee.stacc.productivity.edsl.cache/src/ee/stacc/productivity/edsl/cache/CacheService.java
+++ ee.stacc.productivity.edsl.cache/src/ee/stacc/productivity/edsl/cache/CacheService.java
@@ -4,8 +4,8 @@ package ee.stacc.productivity.edsl.cache;
 
 public class CacheService {
 	
-//	private final static ICacheService SERVICE  = new CacheServiceImpl(new WorkspaceBasedHSQLDBLayer());
-	private final static ICacheService SERVICE  = new CacheServiceImpl(new FilesystemBasedHSQLDBLayer());
+	private final static ICacheService SERVICE  = new CacheServiceImpl(new WorkspaceBasedHSQLDBLayer());
+//	private final static ICacheService SERVICE  = new CacheServiceImpl(new FilesystemBasedHSQLDBLayer());
 	
 	public static ICacheService getCacheService() {
 		return SERVICE;
