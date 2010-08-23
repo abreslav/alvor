6019595615fb9edd454096acaac4c655a67977f7
Carl-Johan Sveningsson <cj@sveningsson.info>
2010-08-23 19:29:37 +0300

    Some way towards proper model management...

--- ee.stacc.productivity.edsl.gui/src/ee/stacc/productivity/edsl/gui/AlvorPropertiesEditor.java
+++ ee.stacc.productivity.edsl.gui/src/ee/stacc/productivity/edsl/gui/AlvorPropertiesEditor.java
@@ -6,38 +6,71 @@ import java.util.Map;
 
 import org.eclipse.core.runtime.IProgressMonitor;
 import org.eclipse.jface.dialogs.ErrorDialog;
-import org.eclipse.swt.SWT;
-import org.eclipse.swt.custom.StyledText;
-import org.eclipse.swt.events.SelectionAdapter;
-import org.eclipse.swt.events.SelectionEvent;
-import org.eclipse.swt.graphics.Font;
-import org.eclipse.swt.graphics.FontData;
-import org.eclipse.swt.layout.FillLayout;
-import org.eclipse.swt.layout.GridData;
-import org.eclipse.swt.layout.GridLayout;
-import org.eclipse.swt.widgets.Button;
+import org.eclipse.swt.events.ModifyEvent;
+import org.eclipse.swt.events.ModifyListener;
 import org.eclipse.swt.widgets.Composite;
-import org.eclipse.swt.widgets.FontDialog;
 import org.eclipse.swt.widgets.Label;
 import org.eclipse.swt.widgets.Text;
 import org.eclipse.ui.IEditorInput;
 import org.eclipse.ui.IEditorPart;
 import org.eclipse.ui.IFileEditorInput;
 import org.eclipse.ui.PartInitException;
-import org.eclipse.ui.editors.text.TextEditor;
 import org.eclipse.ui.forms.IManagedForm;
 import org.eclipse.ui.forms.editor.FormEditor;
 import org.eclipse.ui.forms.editor.FormPage;
+import org.eclipse.ui.forms.events.ExpansionAdapter;
+import org.eclipse.ui.forms.events.ExpansionEvent;
 import org.eclipse.ui.forms.widgets.FormToolkit;
-import org.eclipse.ui.forms.widgets.ScrolledForm;
+import org.eclipse.ui.forms.widgets.Section;
 import org.eclipse.ui.forms.widgets.TableWrapData;
 import org.eclipse.ui.forms.widgets.TableWrapLayout;
-
+import org.eclipse.ui.forms.SectionPart;
 import ee.stacc.productivity.edsl.main.OptionLoader;
 
 public class AlvorPropertiesEditor extends FormEditor {
-	private TextEditor editor;
+//	private boolean isDirty = false;
+	private Map<String, Object> props = null;
 
+	private class AlvorPropertiesSection extends SectionPart {
+		AlvorPropertiesSection(Composite parent,
+				FormToolkit toolkit,
+				int style) {
+			super(parent, toolkit, style);
+			Section section = getSection();
+			
+			section.setText("Section title");
+			section.setDescription("This is the description that goes "+
+			"below the title");
+
+			Composite sectionClient = toolkit.createComposite(section);
+			TableWrapLayout layout = new TableWrapLayout();
+			layout.numColumns = 2;
+			sectionClient.setLayout(layout);
+
+			Label label = toolkit.createLabel(sectionClient, "barbar"); //$NON-NLS-1$
+			TableWrapData td = new TableWrapData(TableWrapData.FILL_GRAB);
+			td.colspan = 2;
+			label.setLayoutData(td);
+
+			Map<String, Object> props = loadPropertiesFromEditorInput();
+
+			for (Map.Entry<String, Object> entry : props.entrySet()) {
+				td = new TableWrapData(TableWrapData.FILL_GRAB);
+				label = toolkit.createLabel(sectionClient, entry.getKey() +":"); //$NON-NLS-1$
+				td = new TableWrapData(TableWrapData.RIGHT);
+				label.setLayoutData(td);
+				td = new TableWrapData(TableWrapData.FILL_GRAB);
+				Text text = toolkit.createText(sectionClient, entry.getValue().toString()); //$NON-NLS-1$
+				text.setLayoutData(td);
+//				text.addModifyListener(new ModifyListener() {
+//					public void modifyText(ModifyEvent e) {
+//						setDirty(true);
+//					}
+//				});
+			}
+		}
+	}
+	
 	private class AlvorPropertiesPage extends FormPage {
 		public static final String PAGE_ID = "properties"; //$NON-NLS-1$
 
@@ -48,36 +81,35 @@ public class AlvorPropertiesEditor extends FormEditor {
 		protected void createFormContent(IManagedForm managedForm) {
 			FormToolkit toolkit = managedForm.getToolkit();
 			Composite composite = managedForm.getForm().getBody();
-			TableWrapLayout layout = new TableWrapLayout();
-			composite.setLayout(layout);
+			composite.setLayout(new TableWrapLayout());
+
+			Label label = toolkit.createLabel(composite, "foobar"); //$NON-NLS-1$
+			label.setLayoutData(new TableWrapData(TableWrapData.FILL_GRAB));
 			
-			ScrolledForm form = toolkit.createScrolledForm(composite);
-			layout = new TableWrapLayout();
-			layout.numColumns = 2;
-			form.getBody().setLayout(layout);
-
-			TableWrapData td = new TableWrapData(TableWrapData.FILL_GRAB);
-			Label label = toolkit.createLabel(form.getBody(), "foobar"); //$NON-NLS-1$
-			td.colspan = 2;
-			label.setLayoutData(td);
-
-			Map<String, Object> props = loadPropertiesFromEditorInput();
-
-			for (Map.Entry<String, Object> entry : props.entrySet()) {
-				td = new TableWrapData(TableWrapData.FILL_GRAB);
-				label = toolkit.createLabel(form.getBody(), entry.getKey() +":"); //$NON-NLS-1$
-				td = new TableWrapData(TableWrapData.RIGHT);
-				label.setLayoutData(td);
-				td = new TableWrapData(TableWrapData.FILL_GRAB);
-				Text text = toolkit.createText(form.getBody(), entry.getValue().toString()); //$NON-NLS-1$
-				text.setLayoutData(td);
-			}
+			AlvorPropertiesSection section = new AlvorPropertiesSection(composite, toolkit,
+					Section.DESCRIPTION|Section.TITLE_BAR|Section.TWISTIE|Section.EXPANDED);
+			section.getSection().setLayoutData(new TableWrapData(TableWrapData.FILL_GRAB));
+			
+//			section.addExpansionListener(new ExpansionAdapter() {
+				//				public void expansionStateChanged(ExpansionEvent e) {
+				//					this.managedForm.reflow(true);
+				//				}
+				//			});
+				
+			managedForm.addPart(section);
+			
+			//TODO Later
+			//// assuming 'editorPart' is an instance of an org.eclipse.ui.IEditorPart
+//			ITextEditor editor = (ITextEditor) editorPart.getAdapter(ITextEditor.class):
+//			if (editor != null) {
+//			  IDocumentProvider provider = editor.getDocumentProvider();
+//			  IDocument document = provider.getDocument(editor.getEditorInput());
+//			}
 		}
 	}
 
 	private Map<String, Object> loadPropertiesFromEditorInput() {
-		Map<String, Object> props = null;
-		
+	
 		try {
 			// TODO We should use something like this to init this page?
 			//			public void init(IEditorSite site, IEditorInput editorInput)
@@ -111,9 +143,9 @@ public class AlvorPropertiesEditor extends FormEditor {
 	protected void addPages() {
 		try {
 			addPage(new AlvorPropertiesPage(this));
-			editor = new TextEditor();
-			int index = addPage(editor, getEditorInput());
-			setPageText(index, editor.getTitle());
+//			editor = new TextEditor();
+//			int index = addPage(editor, getEditorInput());
+//			setPageText(index, editor.getTitle());
 		} catch  (PartInitException e) {
 //			ErrorDialog.openError(
 //					getSite().getShell(),
@@ -125,6 +157,8 @@ public class AlvorPropertiesEditor extends FormEditor {
 	
 	@Override
 	public void doSave(IProgressMonitor monitor) {
+		commitPages(true);
+		editorDirtyStateChanged();
 		getEditor(1).doSave(monitor);
 	}
 
@@ -141,6 +175,15 @@ public class AlvorPropertiesEditor extends FormEditor {
 		return true;
 	}
 
+	private void setDirty(boolean dirty) {
+//		this.isDirty = dirty;
+		firePropertyChange(PROP_DIRTY);
+	}
+
+//	public boolean isDirty() {
+//		return this.isDirty;
+//	}
 }
+
 
 
