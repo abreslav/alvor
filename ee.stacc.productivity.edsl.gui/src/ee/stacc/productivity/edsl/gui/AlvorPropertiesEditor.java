208cc3b1c1a713b64ce83e0463a5338a1dc9105f
Carl-Johan Sveningsson <cj@sveningsson.info>
2010-08-27 11:20:34 +0300

    Finally something which works\!

--- ee.stacc.productivity.edsl.gui/src/ee/stacc/productivity/edsl/gui/AlvorPropertiesEditor.java
+++ ee.stacc.productivity.edsl.gui/src/ee/stacc/productivity/edsl/gui/AlvorPropertiesEditor.java
@@ -17,151 +17,21 @@ import org.eclipse.jface.dialogs.ErrorDialog;
 //import org.eclipse.swt.events.ModifyListener;
 //import org.eclipse.swt.layout.GridData;
 
-import org.eclipse.swt.widgets.Composite;
-import org.eclipse.swt.widgets.Label;
-import org.eclipse.swt.widgets.Text;
 import org.eclipse.ui.IEditorInput;
 import org.eclipse.ui.IEditorPart;
 import org.eclipse.ui.IFileEditorInput;
 import org.eclipse.ui.PartInitException;
-import org.eclipse.ui.forms.IManagedForm;
 import org.eclipse.ui.forms.editor.FormEditor;
-import org.eclipse.ui.forms.editor.FormPage;
 //import org.eclipse.ui.forms.events.ExpansionAdapter;
 //import org.eclipse.ui.forms.events.ExpansionEvent;
-import org.eclipse.ui.forms.widgets.FormToolkit;
-import org.eclipse.ui.forms.widgets.ScrolledForm;
-import org.eclipse.ui.forms.widgets.Section;
-import org.eclipse.ui.forms.widgets.TableWrapData;
-import org.eclipse.ui.forms.widgets.TableWrapLayout;
-import org.eclipse.ui.forms.SectionPart;
 //import org.eclipse.ui.model.WorkbenchLabelProvider;
 
 import ee.stacc.productivity.edsl.main.OptionLoader;
 
-import org.eclipse.pde.core.IModelChangedEvent;
-import org.eclipse.pde.core.IModelChangedListener;
-//import org.eclipse.pde.core.build.IBuildModel;
 
 public class AlvorPropertiesEditor extends FormEditor {
 //	private boolean isDirty = false;
 	private Map<String, Object> props = null;
-	
-	
-	private class AlvorPropertiesSection extends SectionPart implements IModelChangedListener {
-		private FormPage page;
-		
-		AlvorPropertiesSection(FormPage page, Composite parent) {
-			super(parent, page.getManagedForm().getToolkit(), Section.DESCRIPTION|Section.TITLE_BAR|Section.TWISTIE|Section.EXPANDED);
-			this.page = page;
-			
-			getSection().setText("Section title");
-			getSection().setDescription("This is the description that goes below the title");
-
-//			getBuildModel().addModelChangedListener(this);
-			createClient(getSection(), page.getManagedForm().getToolkit());
-		}
-	
-//		,
-//		This has to be implemented without InputContext
-//		private IBuildModel getBuildModel() {
-//			InputContext context = getPage().getPDEEditor().getContextManager().findContext(BuildInputContext.CONTEXT_ID);
-//			if (context == null)
-//				return null;
-//			return (IBuildModel) context.getModel();
-//		}
-
-		public void createClient(final Section section, FormToolkit toolkit) {
-			Composite container = toolkit.createComposite(section);
-			TableWrapLayout layout = new TableWrapLayout();
-			layout.numColumns = 2;
-			container.setLayout(layout);
-			
-			//				fBuildModel = getBuildModel();
-
-			Label label = toolkit.createLabel(container, "barbar"); //$NON-NLS-1$
-			TableWrapData td = new TableWrapData(TableWrapData.FILL_GRAB);
-			td.colspan = 2;
-			label.setLayoutData(td);
-
-			label = toolkit.createLabel(container, "epan:"); //$NON-NLS-1$
-			td = new TableWrapData(TableWrapData.FILL_GRAB|TableWrapData.RIGHT);
-			label.setLayoutData(td);
-
-			Text text = toolkit.createText(container, "bepan"); //$NON-NLS-1$
-			td = new TableWrapData(TableWrapData.FILL_GRAB);
-			text.setLayoutData(td);			
-			
-//			Map<String, Object> props = loadPropertiesFromEditorInput();
-//
-//			for (Map.Entry<String, Object> entry : props.entrySet()) {
-//				label = toolkit.createLabel(sectionClient, entry.getKey() +":"); //$NON-NLS-1$
-//				td = new TableWrapData(TableWrapData.FILL_GRAB|TableWrapData.RIGHT);
-//				label.setLayoutData(td);
-//				Text text = toolkit.createText(sectionClient, entry.getValue().toString()); //$NON-NLS-1$
-//				td = new TableWrapData(TableWrapData.FILL_GRAB);
-//				text.setLayoutData(td);
-//			/*	text.addModifyListener(new ModifyListener() {
-//					public void modifyText(ModifyEvent e) {
-//						setDirty(true);
-//					}
-//				});*/
-//			}
-
-			section.setLayoutData(new TableWrapData(TableWrapData.FILL_GRAB));
-			section.setClient(container);
-		}
-
-		@Override
-		public void modelChanged(IModelChangedEvent event) {
-			// TODO Auto-generated method stub
-			
-		}
-	}
-	
-	
-	
-	
-	private class AlvorPropertiesPage extends FormPage {
-		public static final String PAGE_ID = "properties"; //$NON-NLS-1$
-		private AlvorPropertiesEditor.AlvorPropertiesSection section;
-
-		public AlvorPropertiesPage(FormEditor editor) {
-			super(editor, PAGE_ID, "Alvor configuration");
-		}
-
-		protected void createFormContent(IManagedForm mform) {
-			super.createFormContent(mform);
-			FormToolkit toolkit = mform.getToolkit();
-			ScrolledForm form = mform.getForm();
-			form.getBody().setLayout(new TableWrapLayout());	
-//			form.setText(PDEUIMessages.BuildEditor_BuildPage_title);
-
-			Label label = toolkit.createLabel(form.getBody(), "foobar"); //$NON-NLS-1$
-			label.setLayoutData(new TableWrapData(TableWrapData.FILL_GRAB));
-						
-			section = new AlvorPropertiesEditor.AlvorPropertiesSection(this, form.getBody());
-			
-			// TODO: We want to keep it expanded all the time...
-			//			section.addExpansionListener(new ExpansionAdapter() {
-			//				public void expansionStateChanged(ExpansionEvent e) {
-			//					this.managedForm.reflow(true);
-			//				}
-			//			});		
-
-			mform.addPart(section);
-			
-			// TODO IDocument management later
-			//// assuming 'editorPart' is an instance of an org.eclipse.ui.IEditorPart
-			//			ITextEditor editor = (ITextEditor) editorPart.getAdapter(ITextEditor.class):
-			//			if (editor != null) {
-			//			  IDocumentProvider provider = editor.getDocumentProvider();
-			//			  IDocument document = provider.getDocument(editor.getEditorInput());
-			//			}
-		}
-	}
-
-	
 	
 	private Map<String, Object> loadPropertiesFromEditorInput() {
 		try {
