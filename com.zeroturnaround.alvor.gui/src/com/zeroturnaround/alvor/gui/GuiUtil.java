80ec6c6d1bc951b59593621be45b23fc828b0bdf
Aivar Annamaa <aivar.annamaa@gmail.com>
2010-09-06 18:43:09 +0300

    gui tests + alt-cache

--- com.zeroturnaround.alvor.gui/src/com/zeroturnaround/alvor/gui/GuiUtil.java
+++ com.zeroturnaround.alvor.gui/src/com/zeroturnaround/alvor/gui/GuiUtil.java
@@ -3,7 +3,10 @@ package com.zeroturnaround.alvor.gui;
 import java.util.ArrayList;
 import java.util.List;
 
+import org.eclipse.core.runtime.IStatus;
+import org.eclipse.core.runtime.Status;
 import org.eclipse.jdt.core.IJavaElement;
+import org.eclipse.jface.dialogs.ErrorDialog;
 import org.eclipse.jface.viewers.ISelection;
 import org.eclipse.jface.viewers.StructuredSelection;
 import org.eclipse.ui.PlatformUI;
@@ -25,5 +28,30 @@ public class GuiUtil {
 		throw new IllegalStateException("No Java element selected");
 	}
 
-
+	public static IJavaElement getSingleSelectedJavaElement() throws IllegalAccessException {
+		ISelection selection = PlatformUI.getWorkbench().getActiveWorkbenchWindow().getSelectionService().getSelection();
+		if (selection instanceof StructuredSelection) {
+			StructuredSelection structSel = (StructuredSelection) selection;
+			if (structSel.size() != 1) {
+				throw new IllegalAccessException("One element should be selected");
+			}
+			else {
+				Object sel = structSel.iterator().next();
+				if (sel instanceof IJavaElement) {
+					return (IJavaElement)sel;
+				}
+				else {
+					throw new IllegalAccessException("Expected a Java element");
+				}
+			}
+		}
+		throw new IllegalAccessException("Can't find selected Java element");
+	}
+	
+	public static void ShowErrorDialog(String msg) {
+		IStatus status = new Status(IStatus.ERROR, 
+				"com.zeroturnaround.alvor.gui", 1, "Error", null);
+		ErrorDialog.openError(PlatformUI.getWorkbench().getActiveWorkbenchWindow().getShell(), 
+			      "This is your final warning", null, status);
+	}
 }
