8172ac6281fa49001adb628f36d6a582e24b1e42
Aivar Annamaa <aivar.annamaa@gmail.com>
2010-11-02 15:59:21 +0200

    fixed disabled dynamic checking case

--- com.zeroturnaround.alvor.common/src/com/zeroturnaround/alvor/common/logging/Logs.java
+++ com.zeroturnaround.alvor.common/src/com/zeroturnaround/alvor/common/logging/Logs.java
@@ -7,10 +7,15 @@ import java.util.Map;
 public class Logs {
 	private static Map<String, ILog> logMap = new HashMap<String, ILog>();
 	public static ILog getLog(Class<?> clazz) {
-		String name = clazz.getCanonicalName(); 
+		return getLog(clazz.getCanonicalName()); 
+	}
+	
+	public static ILog getLog(String name) {
 		if (!logMap.containsKey(name)) {
 			logMap.put(name, new LogImpl(name));
 		}
 		return logMap.get(name);
 	}
+	
+	
 }
