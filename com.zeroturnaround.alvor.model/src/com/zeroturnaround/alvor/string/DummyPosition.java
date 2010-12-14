52880b29252b4cf41e346e06fecd94b3eb5a7c34
Aivar Annamaa <aivar.annamaa@gmail.com>
2010-12-14 14:07:47 +0200

    fixed problem with "cloned" DummyPosition-s

--- com.zeroturnaround.alvor.model/src/com/zeroturnaround/alvor/string/DummyPosition.java
+++ com.zeroturnaround.alvor.model/src/com/zeroturnaround/alvor/string/DummyPosition.java
@@ -2,7 +2,12 @@ package com.zeroturnaround.alvor.string;
 
 
 public class DummyPosition extends Position {
+	public static final String DUMMY_PATH = "__dummy__"; 
 	public DummyPosition() {
-		super("__dummy__", 1, 2);
+		super(DummyPosition.DUMMY_PATH , 1, 2);
+	}
+	public static boolean isDummyPosition(IPosition pos) {
+		return (pos instanceof DummyPosition)
+			|| pos.getPath().equals(DummyPosition.DUMMY_PATH);
 	}
 }
