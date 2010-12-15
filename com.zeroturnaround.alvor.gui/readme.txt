0d95ac49037474f5ed1c774bc52ebc2b510b07cb
Aivar Annamaa <aivar.annamaa@gmail.com>
2010-12-15 18:34:41 +0200

    started new conf editor

--- com.zeroturnaround.alvor.gui/readme.txt
+++ com.zeroturnaround.alvor.gui/readme.txt
@@ -15,3 +15,6 @@ TODO: create progress-bar
 
 SQL search feature (instead of all-hotspots view)
 
+
+when automatic checking is re-enabled, then all changes made in the meanwhile are processed,
+and further changes will be processed automatically
---
+++ com.zeroturnaround.alvor.gui/src/com/zeroturnaround/alvor/gui/configuration/AlvorXmlEditorContributor.java
@@ -0,0 +1,10 @@
+package com.zeroturnaround.alvor.gui.configuration;
+
+import org.eclipse.ui.part.EditorActionBarContributor;
+
+public class AlvorXmlEditorContributor extends EditorActionBarContributor {
+
+	public AlvorXmlEditorContributor() {
+	}
+
+}
