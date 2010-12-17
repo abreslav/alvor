a6e422f61de115658ace7d5fa7cccb4abd02c24d
Aivar Annamaa <aivar.annamaa@gmail.com>
2010-12-17 14:57:21 +0200

    more to editor + help + prefs

---
+++ com.zeroturnaround.alvor.gui/src/com/zeroturnaround/alvor/gui/configuration/AlvorPreferencePage.java
@@ -0,0 +1,26 @@
+package com.zeroturnaround.alvor.gui.configuration;
+
+import org.eclipse.jface.preference.PreferencePage;
+import org.eclipse.swt.SWT;
+import org.eclipse.swt.widgets.Composite;
+import org.eclipse.swt.widgets.Control;
+import org.eclipse.swt.widgets.Label;
+import org.eclipse.ui.IWorkbench;
+import org.eclipse.ui.IWorkbenchPreferencePage;
+
+public class AlvorPreferencePage extends PreferencePage implements IWorkbenchPreferencePage {
+
+	@Override
+	protected Control createContents(Composite parent) {
+		noDefaultAndApplyButton();
+		
+		Label label = new Label(parent, SWT.WRAP);
+		label.setText("Alvor is configured project-wise. Right-click on project and ...");
+		return null;
+	}
+
+	@Override
+	public void init(IWorkbench workbench) {
+	}
+
+}
--- com.zeroturnaround.alvor.gui/src/com/zeroturnaround/alvor/gui/configuration/AlvorXmlEditor.java
+++ com.zeroturnaround.alvor.gui/src/com/zeroturnaround/alvor/gui/configuration/AlvorXmlEditor.java
@@ -1,14 +1,22 @@
 package com.zeroturnaround.alvor.gui.configuration;
 import org.eclipse.core.runtime.IProgressMonitor;
-import org.eclipse.swt.widgets.Composite;
-import org.eclipse.ui.IEditorInput;
-import org.eclipse.ui.IEditorSite;
 import org.eclipse.ui.PartInitException;
-import org.eclipse.ui.part.EditorPart;
+import org.eclipse.ui.PlatformUI;
+import org.eclipse.ui.editors.text.TextEditor;
+import org.eclipse.ui.forms.editor.FormEditor;
+import org.eclipse.ui.forms.widgets.FormToolkit;
+
+import com.zeroturnaround.alvor.common.logging.ILog;
+import com.zeroturnaround.alvor.common.logging.Logs;
 
 
-public class AlvorXmlEditor extends EditorPart {
+public class AlvorXmlEditor extends FormEditor {
+	private static final ILog LOG = Logs.getLog(AlvorXmlEditor.class);
+	private FormToolkit toolkit = new FormToolkit(PlatformUI.getWorkbench().getDisplay());
+	AlvorXmlEditorMainPage mainPage = new AlvorXmlEditorMainPage(this, toolkit);
+	TextEditor texteditor = new TextEditor();
 
+	
 	public AlvorXmlEditor() {
 	}
 
@@ -21,26 +29,30 @@ public class AlvorXmlEditor extends EditorPart {
 	}
 
 	@Override
-	public void init(IEditorSite site, IEditorInput input)
-			throws PartInitException {
-	}
-
-	@Override
 	public boolean isDirty() {
-		return false;
+		return false; //texteditor != null && texteditor.isDirty();
 	}
 
 	@Override
 	public boolean isSaveAsAllowed() {
-		return false;
-	}
-
-	@Override
-	public void createPartControl(Composite parent) {
+		return true;
 	}
 
 	@Override
 	public void setFocus() {
+		mainPage.setFocus();
+	}
+
+	@Override
+	protected void addPages() {
+		try {
+			this.addPage(mainPage);
+			
+			int index = addPage(texteditor, getEditorInput());
+			setPageText(index, texteditor.getTitle());
+		} catch (PartInitException e) {
+			LOG.exception(e);
+		}
 	}
 
 }
