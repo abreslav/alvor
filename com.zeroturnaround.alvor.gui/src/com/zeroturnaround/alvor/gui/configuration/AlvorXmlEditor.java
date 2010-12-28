112304b4a34e3b52881a26731a4aa1a847e5ba6d
Aivar Annamaa <aivar.annamaa@gmail.com>
2010-12-28 18:14:01 +0200

    configuration trial with Table and TableViewer

--- com.zeroturnaround.alvor.gui/src/com/zeroturnaround/alvor/gui/configuration/AlvorXmlEditor.java
+++ com.zeroturnaround.alvor.gui/src/com/zeroturnaround/alvor/gui/configuration/AlvorXmlEditor.java
@@ -1,36 +1,50 @@
 package com.zeroturnaround.alvor.gui.configuration;
 import org.eclipse.core.runtime.IProgressMonitor;
+import org.eclipse.ui.IEditorInput;
 import org.eclipse.ui.PartInitException;
 import org.eclipse.ui.PlatformUI;
 import org.eclipse.ui.editors.text.TextEditor;
 import org.eclipse.ui.forms.editor.FormEditor;
 import org.eclipse.ui.forms.widgets.FormToolkit;
+import org.eclipse.ui.part.FileEditorInput;
 
 import com.zeroturnaround.alvor.common.logging.ILog;
 import com.zeroturnaround.alvor.common.logging.Logs;
+import com.zeroturnaround.alvor.configuration.ConfigurationManager;
+import com.zeroturnaround.alvor.configuration.ProjectConfiguration;
 
+/**
+ * @author Aivar
+ *
+ */
 
 public class AlvorXmlEditor extends FormEditor {
 	private static final ILog LOG = Logs.getLog(AlvorXmlEditor.class);
 	private FormToolkit toolkit = new FormToolkit(PlatformUI.getWorkbench().getDisplay());
-	AlvorXmlEditorMainPage mainPage = new AlvorXmlEditorMainPage(this, toolkit);
-	TextEditor texteditor = new TextEditor();
+	private AlvorXmlEditorFormPage formPage;
+	private TextEditor texteditor;
+	private ProjectConfiguration configuration;
 
 	
 	public AlvorXmlEditor() {
+		super();
 	}
-
+	
 	@Override
 	public void doSave(IProgressMonitor monitor) {
+		texteditor.doSave(monitor);
+		formPage.doSave(monitor);
 	}
 
 	@Override
 	public void doSaveAs() {
+		
 	}
 
 	@Override
 	public boolean isDirty() {
-		return false; //texteditor != null && texteditor.isDirty();
+		return texteditor != null && texteditor.isDirty()
+			|| formPage != null && formPage.isDirty();
 	}
 
 	@Override
@@ -40,19 +54,32 @@ public class AlvorXmlEditor extends FormEditor {
 
 	@Override
 	public void setFocus() {
-		mainPage.setFocus();
+		formPage.setFocus();
 	}
 
 	@Override
 	protected void addPages() {
 		try {
-			this.addPage(mainPage);
+			assert(this.configuration != null); // setInput is called before addPages
 			
+			// form page
+			formPage = new AlvorXmlEditorFormPage(this, toolkit, configuration);
+			this.addPage(formPage);
+			
+			// text-editor page
+			texteditor = new TextEditor();
 			int index = addPage(texteditor, getEditorInput());
 			setPageText(index, texteditor.getTitle());
-		} catch (PartInitException e) {
+		}
+		catch (PartInitException e) {
 			LOG.exception(e);
 		}
 	}
-
+	
+	@Override
+	protected void setInput(IEditorInput input) {
+		super.setInput(input);
+		FileEditorInput inp = (FileEditorInput) input;
+		this.configuration = ConfigurationManager.loadFromFile(inp.getPath().toFile());
+	}
 }
