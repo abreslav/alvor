86cc03703aea0d08097bbb3e533a33b3629b7a28
aivar.annamaa <aivar.annamaa@localhost>
2011-06-09 16:49:16 +0300

    changes in connection tracker

--- com.googlecode.alvor.crawler/src/com/googlecode/alvor/conntracker/ConnectionTracker.java
+++ com.googlecode.alvor.crawler/src/com/googlecode/alvor/conntracker/ConnectionTracker.java
@@ -5,16 +5,18 @@ import org.eclipse.jdt.core.dom.IBinding;
 import org.eclipse.jdt.core.dom.IVariableBinding;
 import org.eclipse.jdt.core.dom.MethodInvocation;
 import org.eclipse.jdt.core.dom.Name;
+import org.eclipse.jdt.core.dom.StringLiteral;
+
+import com.googlecode.alvor.crawler.util.ASTUtil;
 
 
 public class ConnectionTracker {
 	public static String getConnectionDescriptor(Expression node) {
 		if (node == null) {
-			System.err.println("CONN NULL");
+			return "";
 		}
 		else if (node instanceof MethodInvocation) {
-			// create string describing this method invocation
-			System.err.println("CONN INV: " + node);
+			return getPatternStringForInvocation((MethodInvocation)node);
 		}
 		else if (node instanceof Name) {
 			Name name = (Name)node;
@@ -22,22 +24,66 @@ public class ConnectionTracker {
 			if (binding instanceof IVariableBinding) {
 				IVariableBinding var = (IVariableBinding)name.resolveBinding();
 				if (var.isField()) {
-					System.err.println("CONN FIELD: " + var);
+					return getPatternStringForField(var);
 				}
 				else if (var.isParameter()) {
-					System.err.println("CONN PARAM: " + var);
+					// TODO
+					return var.getName();
 				}
 				else {
-					return "?"; // will be checked against all databases
-				// 	TODO try to track down assignments
-				//	NameUsage usage = VariableTracker.getLastMod((Name)node);
+					return getPatternStringForVariable(var);
 				}
 			} else {
-				System.err.println("NONVAR name binding: " + binding.getName());
+				return "";
+			}
+		}
+		else {
+			return "";
+		}
+	}
+	
+	/**
+	 * 
+	 * @param inv
+	 * @return sample: qualified.class.name(_,_,"string literal")
+	 */
+	private static String getPatternStringForInvocation(MethodInvocation inv) {
+		StringBuilder result = new StringBuilder();
+		Expression exp = inv.getExpression();
+		if (exp != null) {
+			result.append(exp.resolveTypeBinding().getQualifiedName()); 
+		}
+		else {
+			result.append(ASTUtil.getContainingTypeDeclaration(inv).resolveBinding().getQualifiedName()); 
+		}
+		
+		result.append("." + inv.getName());
+		result.append("(");
+		for (int i=0; i < inv.arguments().size(); i++) {
+			if (i > 0) {
+				result.append(",");
+			}
+			Expression arg = (Expression)inv.arguments().get(i); 
+			if (arg instanceof StringLiteral) {
+				result.append(((StringLiteral) arg).getEscapedValue());
+			}
+			else {
+				result.append("_");
 			}
 		}
 		
-		return "*";
+		return result.toString();
+	}
+	
+	private static String getPatternStringForField(IVariableBinding field) {
+		// TODO
+		return field.getName();
+	}
+	
+	private static String getPatternStringForVariable(IVariableBinding var) {
+		// 	TODO try to track down assignments
+		//	NameUsage usage = VariableTracker.getLastMod((Name)node);
+		return var.getName();
 	}
 	
 }
